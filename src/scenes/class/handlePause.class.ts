ce0cfb9beb34166a9fcbd562649219ee20b15755
byManuel18 <antoniomanu533@gmail.com>
2024-08-16 20:04:20 +0200

    change play pause song

--- src/scenes/class/handlePause.class.ts
+++ src/scenes/class/handlePause.class.ts
@@ -5,11 +5,15 @@ import { GlobalScene } from "./globalScene.class";
 
 export class HandlePause {
 
+    pauseSong: Phaser.Sound.NoAudioSound | Phaser.Sound.HTML5AudioSound | Phaser.Sound.WebAudioSound;
+
 
     constructor(scene: GlobalScene){
+        this.pauseSong = scene.sound.add(AUDIO.pause.key);
+
         scene.input?.keyboard?.on('keydown-ENTER', () => {
 
-            scene.sound.add(AUDIO.pause.key).play({volume: 0.5});
+            this.pauseSong.play({volume: 0.1});
 
             if(scene.physics.world.isPaused){
                 scene.physics.world.resume();
