aaaeae9c2e3a7ba33f6f2ff99a30afb7bf78d1b5
byManuel18 <antoniomanu533@gmail.com>
2024-11-17 16:33:44 +0100

    refactor audios and animations

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -1,137 +1,72 @@
-import { SPRITES } from './../entities/config/sprites';
+import { SPRITES } from "./../entities/config/sprites";
 import { ANIMATIONS } from "../entities/config/animations";
 import { AUDIO } from "./audio/audio";
 import { SCENES } from "./config/scenes.config";
 
-
 export class PreloadScene extends Phaser.Scene {
+  constructor() {
+    super(SCENES.PRELOAD);
+  }
 
+  preload(): void {
+    this.loadSprites();
+    this.loadAudio();
+    this.loadImages();
+  }
 
-    constructor(){
-        super(SCENES.PRELOAD);
-    }
+  create(): void {
+    this.createAnimations();
 
-    preload():void{
-        this.loadSprites();
-        this.loadAudio();
-        this.loadImages();
-    }
+    this.scene.start(SCENES.LEVEL_ONE);
+  }
 
-    create():void{
-        this.createAnimations();
-      
-        this.scene.start(SCENES.LEVEL_ONE);
-      
-    }
+  private loadSprites() {
+    Object.keys(SPRITES).forEach((keyConst) => {
+      const {
+        key,
+        frames: { frameHeight, frameWidth },
+        path,
+      } = SPRITES[keyConst as keyof typeof SPRITES];
+      this.load.spritesheet(key, path, { frameWidth, frameHeight });
+    });
+  }
 
-
-    private loadSprites(){
-
-        Object.keys(SPRITES).forEach(keyConst=>{
-            const { key, frames: {frameHeight, frameWidth} , path} = SPRITES[keyConst as keyof typeof SPRITES];
-            this.load.spritesheet(
-                key,
-                path,
-                { frameWidth, frameHeight },
-            );
-        });
-    }
-
-    private createAnimations(){
-        this.anims.create({
-            key: ANIMATIONS.mario.walk.key,
-            frames: this.anims.generateFrameNumbers(
-                ANIMATIONS.mario.walk.sprite.key,
-                { 
-                    start: ANIMATIONS.mario.walk.sprite.start, 
-                    end:  ANIMATIONS.mario.walk.sprite.end 
-                }
-            ),
-            frameRate: ANIMATIONS.mario.walk.frameRate,
-            repeat: -1
-        });
+  private createAnimations() {
+    Object.keys(ANIMATIONS).forEach((keyConst) => {
+      const animations = ANIMATIONS[keyConst as keyof typeof ANIMATIONS];
+      Object.keys(animations).forEach((keyAnimConst) => {
+        const {
+          key,
+          sprite: { key: keyAnim, end, start },
+          frameRate,
+        } = animations[keyAnimConst as keyof typeof animations];
 
         this.anims.create({
-            key: ANIMATIONS.mario.idle.key,
-            frames: this.anims.generateFrameNumbers(
-                ANIMATIONS.mario.idle.sprite.key,
-                { 
-                    start: ANIMATIONS.mario.idle.sprite.start, 
-                    end:  ANIMATIONS.mario.idle.sprite.end 
-                }
-            ),
-            frameRate: ANIMATIONS.mario.idle.frameRate,
-            repeat: -1
+          key: key,
+          frames: this.anims.generateFrameNumbers(keyAnim, {
+            start,
+            end,
+          }),
+          frameRate,
+          repeat: -1,
         });
+      });
+    });
+  }
 
-        this.anims.create({
-            key: ANIMATIONS.mario.jump.key,
-            frames: this.anims.generateFrameNumbers(
-                ANIMATIONS.mario.jump.sprite.key,
-                { 
-                    start: ANIMATIONS.mario.jump.sprite.start, 
-                    end:  ANIMATIONS.mario.jump.sprite.end 
-                }
-            ),
-            frameRate: ANIMATIONS.mario.jump.frameRate,
-            repeat: -1
-        });
+  private loadAudio() {
+    Object.keys(AUDIO).forEach((keyAudio) => {
+      const { key, path } = AUDIO[keyAudio as keyof typeof Audio];
+      this.load.audio(key, path);
+    });
+  }
 
-        this.anims.create({
-            key: ANIMATIONS.mario.dead.key,
-            frames: this.anims.generateFrameNumbers(
-                ANIMATIONS.mario.dead.sprite.key,
-                { 
-                    start: ANIMATIONS.mario.dead.sprite.start, 
-                    end:  ANIMATIONS.mario.dead.sprite.end 
-                }
-            ),
-            frameRate: ANIMATIONS.mario.dead.frameRate,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: ANIMATIONS.goomba.walk.key,
-            frames: this.anims.generateFrameNumbers(
-                ANIMATIONS.goomba.walk.sprite.key,
-                { 
-                    start: ANIMATIONS.goomba.walk.sprite.start, 
-                    end:  ANIMATIONS.goomba.walk.sprite.end 
-                }
-            ),
-            frameRate: ANIMATIONS.goomba.walk.frameRate,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: ANIMATIONS.goomba.dead.key,
-            frames: this.anims.generateFrameNumbers(
-                ANIMATIONS.goomba.dead.sprite.key,
-                { 
-                    start: ANIMATIONS.goomba.dead.sprite.start, 
-                    end:  ANIMATIONS.goomba.dead.sprite.end 
-                }
-            ),
-            frameRate: ANIMATIONS.goomba.dead.frameRate,
-            repeat: -1
-        });
-
-        
-    }
-
-    private loadAudio(){
-        this.load.audio(AUDIO.overworldTheme.key,AUDIO.overworldTheme.path);
-        this.load.audio(AUDIO.dead.key,AUDIO.dead.path);
-        this.load.audio(AUDIO.pause.key,AUDIO.pause.path);
-        this.load.audio(AUDIO.marioJump.key,AUDIO.marioJump.path);
-        this.load.audio(AUDIO.goombaStomp.key,AUDIO.goombaStomp.path);
-    }
-
-    private loadImages(){
-        this.load.image("mushroom", "../../assets/collectibles/super-mushroom.png");
-        this.load.image("mushroom-live", "../../assets/collectibles/live-mushroom.png");
-        this.load.image("slot", "../../assets/mine/slot.png");
-    }
-
- 
+  private loadImages() {
+    this.load.image("mushroom", "../../assets/collectibles/super-mushroom.png");
+    this.load.image(
+      "mushroom-live",
+      "../../assets/collectibles/live-mushroom.png"
+    );
+    this.load.image("slot", "../../assets/mine/slot.png");
+  }
 }
